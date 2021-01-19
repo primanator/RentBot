acbf8ebcb535d0e9ee40d43a72b17465bef65a32
Oleksii Prymolonnyi <oprymolonnyi@foundationmedicine.com>
2021-01-19 17:53:36 +0200

    Welcoming message and basic dropdown

--- RentBot/BotClient.cs
+++ RentBot/BotClient.cs
@@ -3,6 +3,8 @@ using System;
 using Telegram.Bot.Types;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Collections.Generic;
 
 namespace RentBot
 {
@@ -24,8 +26,28 @@ namespace RentBot
             if (message.Type == UpdateType.Message)
             {
                 _logger.LogInformation($"Received a text message in chat {message.Message.Chat.Id}.");
-                await _botClient.SendTextMessageAsync(message.Message.Chat, message.Message.Text);
+
+                if (message.Message.Text.Equals("/start", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await _botClient.SendTextMessageAsync(message.Message.Chat.Id, GetWelcomingText(message.Message.From),
+                        replyMarkup: new InlineKeyboardMarkup(new List<List<InlineKeyboardButton>>
+                        {
+                            new List<InlineKeyboardButton> { InlineKeyboardButton.WithCallbackData("How do I get to your place?", "Path") },
+                            new List<InlineKeyboardButton> { InlineKeyboardButton.WithCallbackData("Show me what's around!", "Places") }
+                        }));
+                }
+                else
+                {
+                    await _botClient.SendTextMessageAsync(message.Message.Chat, message.Message.Text);
+                }
             }
+        }
+
+        private string GetWelcomingText(User user)
+        {
+            var welcomeText = $"Hi, {user.FirstName}";
+            welcomeText += string.IsNullOrEmpty(user.LastName) ? string.Empty : " " + user.LastName;
+            return welcomeText + "!";
         }
     }
 }
